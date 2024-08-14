052c90986f7796de0dd159d1997bf9f65510cf6a
mrbes25 <bastian123schmid@gmail.com>
2024-08-14 13:09:46 +0200

    adjusting duplicates

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: bschmid <marvin@42lausanne.ch>             +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/19 11:12:49 by bschmid           #+#    #+#              #
-#    Updated: 2024/03/19 11:12:52 by bschmid          ###   ########.fr        #
+#    Updated: 2024/08/14 12:02:03 by bschmid          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS = $(shell find src/ft_printf src/libft src/GNL -name "*.c")
+SRCS = $(shell find ft_printf libft GNL -name "*.c")
 NAME = my_lib.a
-CFLAGS = -Wall -Werror -Wextra 
+CFLAGS = -Wall -Werror -Wextra
 OBJS = ${SRCS:.c=.o}
 LIBC = ar rcs
 CC = cc
--- my_lib.h
+++ my_lib.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   my_lib.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bschmid <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*   By: bschmid <bschmid@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 11:10:55 by bschmid           #+#    #+#             */
-/*   Updated: 2024/03/19 11:11:14 by bschmid          ###   ########.fr       */
+/*   Created: 2024/08/14 12:00:47 by bschmid           #+#    #+#             */
+/*   Updated: 2024/08/14 12:01:35 by bschmid          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #ifndef MY_LIB_H
 #define MY_LIB_H
 
-#include "src/libft/libft.h"
-#include "src/ft_printf/ft_printf.h"
-#include "src/GNL/get_next_line.h"
+#include "libft/libft.h"
+#include "ft_printf/ft_printf.h"
+#include "GNL/get_next_line.h"
 
 #endif
